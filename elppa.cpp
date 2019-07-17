83da1421e9c169fd8fc0a9b381fd8d1f291e756d
Gautam Kumar <gautamnitc@gmail.com>
2019-07-16 21:26:28 -0700

    Adding Apple

---
+++ elppa.cpp
@@ -0,0 +1,10 @@
+Given a 2D board which has blockers, fires and men. At any given time fires can spread in all 8 directions.  A person can move into 8 directions too. Return true if man can skip the board. 
+
+Given a 2D board which has wells, villages and blockers returns the smallest distance from wells for each village. Remember you have multiple village. 
+
+Design the auto complete search ? 
+How will you solve a coloring the pixel maze. 
+Design a game to complete the word. 
+
+
+
